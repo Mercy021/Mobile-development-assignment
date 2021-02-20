b7327e2bf63121b95e362222bef0353ed16bbf33
mercy021 <kinyanjuimercywangari@gmail.com>
2021-02-20 22:24:55 +0300

    variable assignment

---
+++ src/Variables.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var fullname="Michelle"
+    println(fullname)
+    var age="25"
+    println(age)
+    var phoneNumber="0710898510"
+    println(phoneNumber)
+    var weight:Float=55.4.toFloat()
+    println(weight)
+    var kenyancitizen:Boolean=true
+    println(kenyancitizen)
+
+}
